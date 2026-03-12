efebe1edce8d020c48975641e95d4ad2e327afdf
carlosmartinez050 <martinezreyesc38@gmail.com>
2026-03-11 22:34:25 -0400

    feat(JS): script para el evento en el input

---
+++ static/js/header.js
@@ -0,0 +1,62 @@
+// ======= MENU HAMBURGUESA (MOBILE) ======
+
+const btnHamburguesa = document.getElementById('btn-menu-hamburguesa');
+const menuMovil      = document.getElementById('menu-movil');
+const btnCerrar      = document.getElementById('btn-cerrar-menu');
+
+/* Abrir */
+btnHamburguesa.addEventListener('click', () => {
+    menuMovil.classList.add('activo');
+});
+
+/* Cerrar con la X */
+btnCerrar.addEventListener('click', () => {
+    menuMovil.classList.remove('activo');
+});
+
+/* Cerrar si el usuario hace click fuera del menú */
+menuMovil.addEventListener('click', (e) => {
+    if (e.target === menuMovil) {
+        menuMovil.classList.remove('activo');
+    }
+});
+
+
+// ======= CONTENEDOR INPUT (APARECE AL HACER CLIC EN ICONO) ======
+
+iconoBuscar = document.getElementById('container-logo-search-input');
+categoriasPadre = document.querySelectorAll('.tags-label');
+
+
+iconoBuscar.addEventListener('click', () => {
+    const contenedorInput = document.getElementById('container-search-input');
+    categoriasPadre.forEach(cat => {
+        cat.classList.toggle('oculto');
+    }); console.log(categoriasPadre);
+
+    contenedorInput.classList.toggle('active');     // Muestra u oculta el contenedor del input de búsqueda
+});
+
+// JS PARA EL INPUT DE BÚSQUEDA EN EL HEADER (DESKTOP)
+const inputBusqueda = document.getElementById('input-search');
+
+inputBusqueda.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault(); // Evita que el formulario se envíe automáticamente
+        this.form.submit();    // Envía el formulario manualmente
+    } else if (event.key === 'Escape') {
+        this.value = '';       // Limpia el input si se presiona Escape
+    }
+});
+
+
+// JS PARA EL INPUT DE BÚSQUEDA EN EL HEADER (MOBILE)
+const inputBusquedaMobile = document.getElementById('input-search-mobile');
+const iconoLupaMobile = document.getElementById('icon-search-mobile');
+
+iconoLupaMobile.addEventListener('click', () => {
+    if (inputBusquedaMobile.value.trim() !== '') {
+        inputBusquedaMobile.form.submit(); // Envía el formulario si el input no está vacío
+    }
+});
+
