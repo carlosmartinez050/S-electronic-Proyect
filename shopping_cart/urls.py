f2f55a9eb99397324d3d12689278035b15ec08e4
carlosmartinez050 <martinezreyesc38@gmail.com>
2026-03-20 20:10:07 -0400

    refactor(shop..cart/): mejorar nombres de rutas.

--- shopping_cart/urls.py
+++ shopping_cart/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'cart'
+
 urlpatterns = [
     path('agregar_al_carrito/<int:producto_id>/', views.agregar_al_carrito, name='agregar_al_carrito'),
     path('ver_carrito/', views.ver_carrito, name='ver_carrito'),
-    path('eliminar_producto/<int:producto_id>/', views.eliminar_producto_del_carrito, name="eliminar_producto_del_carrito"),
-    path('aplicar_descuento/', views.aplicar_descuento, name="aplicar_descuento"),
+    path('eliminar_producto/<int:producto_id>/', views.eliminar_del_carrito, name="eliminar_del_carrito"),
     path('finalizar_compra/', views.finalizar_compra, name='finalizar_compra')
     
 ]
