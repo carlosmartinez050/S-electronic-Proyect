2231e031eb29d6c7bf391883bf3b16d99cfd5060
carlosmartinez050 <martinezreyesc38@gmail.com>
2026-04-01 02:18:38 -0400

    correccion de configuraciones faltantes

--- config/settings.py
+++ config/settings.py
@@ -193,3 +193,20 @@ if config('USE_CLOUDINARY', default=False, cast=bool):
             "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
         },
     }
+    
+
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'ERROR',
+    },
+}
